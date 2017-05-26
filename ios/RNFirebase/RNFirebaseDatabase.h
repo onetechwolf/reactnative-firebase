ab756a13e433a80e9b06bbd2ec149d12bd548015
onetechwolf <onetechwolf@gmail.com>
2017-05-26 15:14:19 +0100

    [android] Add internet permission as default Mainifest requirement

--- ios/RNFirebase/RNFirebaseDatabase.h
+++ ios/RNFirebase/RNFirebaseDatabase.h
@@ -1,6 +1,7 @@
 #ifndef RNFirebaseDatabase_h
 #define RNFirebaseDatabase_h
 
+#import "Firebase.h"
 #if __has_include(<React/RCTEventEmitter.h>)
 #import <React/RCTEventEmitter.h>
 #else // Compatibility for RN version < 0.40
@@ -12,18 +13,14 @@
 #import "RCTBridgeModule.h"
 #endif
 
-#if __has_include(<FirebaseDatabase/FIRDatabase.h>)
-#import "Firebase.h"
+@interface RNFirebaseDatabase : RCTEventEmitter <RCTBridgeModule> {
 
-@interface RNFirebaseDatabase : RCTEventEmitter <RCTBridgeModule> {}
+}
+
 @property NSMutableDictionary *dbReferences;
 @property NSMutableDictionary *transactions;
 @property dispatch_queue_t transactionQueue;
-@end
 
-#else
-@interface RNFirebaseDatabase : RCTEventEmitter <RCTBridgeModule> {}
 @end
-#endif
 
 #endif
