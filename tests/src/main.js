c6a569283c96a09a81944cea05c8333fd0c691ee
onetechwolf <onetechwolf@gmail.com>
2017-05-27 15:24:51 +0100

    [ios][admob] added stub modules

--- tests/src/main.js
+++ tests/src/main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
+import { Banner } from './../firebase/modules/admob';
+
 import CoreContainer from './containers/CoreContainer';
 import setupStore from './store/setup';
 import { setupSuites } from './tests/index';
@@ -50,6 +52,7 @@ function bootstrap() {
         return null;
       }
 
+      // return <Banner style={{ width: 100, height: 100, backgroundColor: 'pink'}} />;
       return (
         <Provider store={this.state.store}>
           <CoreContainer />
