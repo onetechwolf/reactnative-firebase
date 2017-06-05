73f92ab8c3a02e7740b45d464a3cba4fb82c4d49
onetechwolf <onetechwolf@gmail.com>
2017-06-05 09:58:44 +0100

    Update package.json description

---
+++ ios/RNFirebase/RNFirebaseAdMob.h
@@ -0,0 +1,12 @@
+#ifndef RNFirebaseAdMob_h
+#define RNFirebaseAdMob_h
+
+#import <React/RCTBridgeModule.h>
+
+@interface RNFirebaseAdMob : NSObject <RCTBridgeModule> {
+
+}
+
+@end
+
+#endif
---
+++ ios/RNFirebase/RNFirebaseAdMob.m
@@ -0,0 +1,10 @@
+#import "RNFirebaseAdMob.h"
+
+@implementation RNFirebaseAdMob
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(log:(NSString *)message) {
+
+}
+
+@end
