fa15077db3ccf6e4f4dc48a8272b21f61aec4394
onetechwolf <onetechwolf@gmail.com>
2017-06-20 10:17:56 +0100

    [docs] Update iOS install documentation

--- tests/src/components/OverviewControlButton.js
+++ tests/src/components/OverviewControlButton.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { PropTypes, Component } from 'react';
 import some from 'lodash.some';
 import { connect } from 'react-redux';
 
