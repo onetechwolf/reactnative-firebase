b6e9ef30d255e18dc562a18c84651ed94a524551
onetechwolf <onetechwolf@gmail.com>
2018-02-11 16:52:14 +0000

    [tests] update tests firebase bootstrap to use `signInAnonymouslyAndRetrieveData`

--- tests/android/app/src/main/java/com/reactnativefirebasedemo/MainApplication.java
+++ tests/android/app/src/main/java/com/reactnativefirebasedemo/MainApplication.java
@@ -12,7 +12,6 @@ import io.invertase.firebase.crash.RNFirebaseCrashPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
-import io.invertase.firebase.instanceid.RNFirebaseInstanceIdPackage;
 import io.invertase.firebase.links.RNFirebaseLinksPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.perf.RNFirebasePerformancePackage;
@@ -48,7 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
           new RNFirebaseCrashlyticsPackage(),
           new RNFirebaseDatabasePackage(),
           new RNFirebaseFirestorePackage(),
-          new RNFirebaseInstanceIdPackage(),
           new RNFirebaseLinksPackage(),
           new RNFirebaseMessagingPackage(),
           new RNFirebasePerformancePackage(),
