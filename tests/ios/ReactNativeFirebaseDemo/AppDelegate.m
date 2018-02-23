e0d6f156ad2832b32aa3ae20108ebbc8acb6412c
onetechwolf <onetechwolf@gmail.com>
2018-02-23 02:59:39 +0000

    [firestore] fix issue with NaN serialization

--- tests/ios/ReactNativeFirebaseDemo/AppDelegate.m
+++ tests/ios/ReactNativeFirebaseDemo/AppDelegate.m
@@ -12,14 +12,12 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <Firebase.h>
-#import <RNFirebaseNotifications.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
-
   NSURL *jsCodeLocation;
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
@@ -36,15 +34,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-  [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-  [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
 }
 
 @end
--- tests/src/firebase.js
+++ tests/src/firebase.js
@@ -7,31 +7,6 @@ import DatabaseContents from './tests/support/DatabaseContents';
 RNfirebase.database.enableLogging(true);
 RNfirebase.firestore.enableLogging(true);
 
-RNfirebase.messaging().onMessage(message => {
-  console.log('got new message: ', message);
-});
-
-RNfirebase.messaging().onTokenRefresh(token => {
-  console.log('got new token: ', token);
-});
-
-const init = async () => {
-  try {
-    await RNfirebase.messaging().requestPermission();
-    const instanceid = await RNfirebase.instanceid().get();
-    console.log('instanceid: ', instanceid);
-    const token = await RNfirebase.messaging().getToken();
-    console.log('token: ', token);
-    const initialMessage = await RNfirebase.messaging().getInitialMessage();
-    console.log('initial message: ', initialMessage);
-    // RNfirebase.instanceid().delete();
-  } catch (error) {
-    console.error('messaging init error:', error);
-  }
-};
-
-init();
-
 const config = {
   apiKey: 'AIzaSyDnVqNhxU0Biit9nCo4RorAh5ulQQwko3E',
   authDomain: 'rnfirebase-b9ad4.firebaseapp.com',
@@ -79,18 +54,22 @@ console.log('RNApps -->', RNfirebase.apps);
 // no need for ready checks
 instances.native
   .auth()
-  .signInAnonymously()
-  .then(user => {
-    console.log('defaultApp user ->', user.toJSON());
+  .signInAnonymouslyAndRetrieveData()
+  .then(credential => {
+    if (credential) {
+      console.log('anotherApp credential ->', credential.user.toJSON());
+    }
   });
 
 // dynamically initialized apps need a ready check
 instances.another.onReady().then(app => {
   app
     .auth()
-    .signInAnonymously()
-    .then(user => {
-      console.log('anotherApp user ->', user.toJSON());
+    .signInAnonymouslyAndRetrieveData()
+    .then(credential => {
+      if (credential) {
+        console.log('anotherApp credential ->', credential.user.toJSON());
+      }
     });
 });
 
