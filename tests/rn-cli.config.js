db59f4d41c2af28003e7346de90eb49735841fdd
onetechwolf <onetechwolf@gmail.com>
2018-09-02 04:47:53 +0100

    [tests] update to detox v8.2.3

--- tests/rn-cli.config.js
+++ tests/rn-cli.config.js
@@ -19,6 +19,10 @@ module.exports = {
   getBlacklistRE() {
     return blacklist([
       new RegExp(`^${escape(resolve(__dirname, '..', 'node_modules'))}\\/.*$`),
+      new RegExp(`^${escape(resolve(__dirname, '..', 'tests'))}\\/.*$`),
+      new RegExp(
+        `^${escape(resolve(__dirname, '..', 'tests', 'node_modules'))}\\/.*$`
+      ),
     ]);
   },
 };
