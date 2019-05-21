f8fcfd5370fc5463539ce0e0c32cd75a91827e34
onetechwolf <onetechwolf@gmail.com>
2019-05-21 20:24:55 +0100

    [app] ensure native apps are initialized before any calls of `firebase.apps`

--- packages/functions/lib/index.js
+++ packages/functions/lib/index.js
@@ -20,7 +20,15 @@ import {
   FirebaseModule,
   getFirebaseRoot,
 } from '@react-native-firebase/app/lib/internal';
-import { isAndroid } from '@react-native-firebase/common';
+
+// import {
+//   isNull,
+//   isObject,
+//   isUndefined,
+//   isString,
+//   isOneOf,
+//   isAlphaNumericUnderscore,
+// } from '@react-native-firebase/common';
 
 import version from './version';
 import HttpsError from './HttpsError';
@@ -80,14 +88,7 @@ class FirebaseFunctionsModule extends FirebaseModule {
   }
 
   useFunctionsEmulator(origin) {
-    let _origin = origin;
-    if (isAndroid && _origin) {
-      if (_origin.startsWith('http://localhost'))
-        _origin = _origin.replace('http://localhost', 'http://10.0.2.2');
-      if (_origin.startsWith('http://127.0.0.1'))
-        _origin = _origin.replace('http://127.0.0.1', 'http://10.0.2.2');
-    }
-    this._useFunctionsEmulatorOrigin = _origin || null;
+    this._useFunctionsEmulatorOrigin = origin || null;
   }
 }
 
